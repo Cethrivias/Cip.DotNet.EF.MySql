079dbf04b095672937ddd2e58db6bf7f0dcf3832
Nikita C <cethrivias@gmail.com>
2022-10-30 12:35:09 +0100

    Migrations

---
+++ src/App/Migrations/20221030111312_Init.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    title = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp", nullable: false, defaultValueSql: "current_timestamp"),
+                    updated_at = table.Column<DateTimeOffset>(type: "timestamp", nullable: false, defaultValueSql: "current_timestamp on update current_timestamp")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_events", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_events_title",
+                table: "events",
+                column: "title")
+                .Annotation("MySql:FullTextIndex", true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events");
+        }
+    }
+}
