97137f25c096277a17f6ca13d650dbac013c93c3
Nikita C <cethrivias@gmail.com>
2022-10-30 12:48:51 +0100

    CRUD

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using App.Domain.Models;
+using App.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+var optionsBuilder = new DbContextOptionsBuilder<BookingsContext>();
+var version = new MySqlServerVersion("8.0");
+var connectionString = "Server=127.0.0.1;Port=3306;Database=bookings;Uid=root;Pwd=testdbpass;";
+optionsBuilder.UseMySql(connectionString, version)
+    .LogTo(Console.WriteLine, LogLevel.Information);
+
+var context = new BookingsContext(optionsBuilder.Options);
+
+// Create
+var @event = new Event("Ted Heeran Concert");
+context.Events.Add(@event);
+await context.SaveChangesAsync();
